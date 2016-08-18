7439894e714c4b8000d97193cf3d22239741b1cf
Erwin Hessing <ejhessing@gmail.com>
2016-08-19 11:52:22 +1200

    added server route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import {Link} from 'react-router'
+import request from 'superagent'
 
 
 export default React.createClass({
+  componentDidMount () {
+      request
+        .get('http://192.168.20.48:3000')
+        .end((err, res) => {
+          if (err) {
+            console.error(err.message)
+            return
+          }
+          this.setState({
+            info: res.body
+          })
+        })
+    },
+
+  getInitialState () {
+    return {
+      ip: [],
+      info: [] }
+  },
   render () {
     return (
       <div className='app-container'>
         <p>Weather</p>
         <div>
           <Link to="/tips">What does this mean?!</Link>
+
         </div>
       </div>
     )
